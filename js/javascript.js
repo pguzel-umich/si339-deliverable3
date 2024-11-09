ead535d220caa6169b42886bd3907cee1b5503f8
pguzel-umich <122236420+pguzel-umich@users.noreply.github.com>
2024-11-09 15:32:08 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,20 @@ document.querySelectorAll('img').forEach(img => {
     console.log("runs")
     img.onerror = function () {
         this.onerror = null;
-        this.src = '../images/default_image.jpg'
-        this.alt = "placeholder image"
+
+        console.log("Path is establishing");
+        const defaultImage = new Image();
+        defaultImage.src = 'images/default_image.jpg';
+
+        defaultImage.onerror = () => {
+            console.log("Rerouting path since no access.");
+            defaultImage.src = '../images/default_image.jpg';
+        };
+
+        this.src = defaultImage.src;
+        this.alt = "placeholder image";
+
+        console.log("Default image success");
     };
 });
 
